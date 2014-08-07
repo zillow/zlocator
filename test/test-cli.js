90a48fcac0538c4f0fa4b67e32c4461a2abdd3c5
Daniel Stockman <daniels@zillow.com>
2014-08-07 10:37:25 -0700

    Add test for cli.version().

--- test/test-cli.js
+++ test/test-cli.js
@@ -1,1 +1,17 @@
 /*global describe, it, expect */
+
+var options = require('../lib/options');
+var cli = require('../lib/cli');
+
+describe('cli', function () {
+    describe('version()', function () {
+        it('returns package.json version', function () {
+            var pkg = require('../package.json');
+            expect(cli.version()).to.equal(pkg.version);
+        });
+    });
+
+    describe('interpret()', function () {
+        it('TODO');
+    });
+});
