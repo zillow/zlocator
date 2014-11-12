f170c4192d2370cf404e67a138264badc55a70aa
Marcus Wu <marcusw@zillow.com>
2014-11-12 15:26:02 -0800

    Add grunt task

---
+++ tasks/zlocator.js
@@ -0,0 +1,17 @@
+module.exports = function(grunt) {
+    'use strict';
+
+    grunt.registerTask('zlocator', 'Building source files with Locator, LocatorYUI, LocatorHandlebars', function () {
+        var started = Date.now();
+        var opts = grunt.config(['zlocator']);
+        require('../bin/zlocator')(opts, this.async())
+            .caught(function (ex) {
+                grunt.fatal(ex.message);
+                grunt.fatal(ex.stack);
+            })
+            .lastly(function () {
+                var duration = (Date.now() - started) / 1000;
+                grunt.log.writeln('Finished in %s second%s.', duration.toFixed(2), (duration != 1 ? 's' : ''));
+            });
+    });
+};
